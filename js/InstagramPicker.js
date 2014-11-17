48c3bf651e47c0440692bc8ec935b359735518d7
Eric King <eric.king@lonelyplanet.com>
2014-11-17 16:41:47 -0600

    Lots of updates.

--- js/InstagramPicker.js
+++ js/InstagramPicker.js
@@ -1,26 +1,36 @@
 function InstagramPicker( clientID, redirectURI )
 {
     PhotoPicker.call(this);
-    this.type        = "instgrampicker";
-    this.batchSize   = 20;
-    this.clientID    = clientID;
-    this.redirectURI = redirectURI;
-    this.accessToken = null;
+    this.type           = "instgrampicker";
+    this.batchSize      = 20;
+    this.clientID       = clientID;
+    this.redirectURI    = redirectURI;
+    this.accessToken    = null;
 }
 
 InstagramPicker.prototype = createObject( PhotoPicker.prototype );
 InstagramPicker.prototype.constructor = InstagramPicker;
 
+InstagramPicker.prototype.init = function()
+{
+    PhotoPicker.prototype.init.call(this);
+};
+
+InstagramPicker.prototype.setContainer = function( container ) {
+    this.container = $(container);
+    return this;
+};
+
 InstagramPicker.prototype.authorizePicker = function()
 {
     if ( ! this.clientID || ! this.redirectURI ) {
-        console.log("Please provide clientID and redirectURI");
+        // console.log("Please provide clientID and redirectURI");
         return false;
     }
 
     var url = "http://api.instagram.com/oauth/authorize/?response_type=token&client_id=" + this.clientID + "&redirect_uri=" + this.redirectURI;
 
-    popup( url, { name: 'instagram', width: 600, height: 450 } );
+    popup( url, { name: "instagram", width: 650, height: 480 } );
 };
 
 InstagramPicker.prototype.setAccessToken = function( token )
@@ -36,7 +46,7 @@ InstagramPicker.prototype.fetchData = function()
             "count": this.batchSize,
             "access_token": this.accessToken
         };
-    console.log("Fetching instagram data");
+    // console.log("Fetching instagram data");
     return this.fetch( endpoint, data );
 };
 
@@ -44,15 +54,15 @@ InstagramPicker.prototype.processData = function(data, status, jqXHR)
 {
     this.nextURL = data.pagination.next_url || false;
 
+    this.loadMoreButton.prop("disabled", this.nextURL === false );
+
     var data = data.data,
         i = 0,
         l = data.length;
 
-    for ( ; i < l ; ++i ) {
-        this.photos[ this.photos.length ] = data[ i ];
+    for ( ; i < l ; ) {
+        this.append( data[ i++ ] );
     }
-
-    console.log(this);
 };
 
 InstagramPicker.prototype.open = function()
@@ -72,7 +82,42 @@ InstagramPicker.prototype.open = function()
     }
 };
 
+InstagramPicker.prototype.getSelectedPhotoURL = function()
+{
+    var photo = this.getSelectedPhoto();
+
+    if ( photo !== false && photo.images.standard_resolution.url ) {
+        return photo.images.standard_resolution.url;
+    }
+
+    console.log("No photo selected");
+
+    return "";
+};
+
 InstagramPicker.prototype.render = function()
 {
-    console.log("Rendering instagram data");
+    // console.log("Rendering instagram data");
+    // this should open the lightbox
+};
+
+InstagramPicker.prototype.append = function( photo )
+{
+    var photos_length = this.photos.push( photo );
+
+    var img = new Image(),
+        item = document.createElement("div");
+
+    img.onload = function() {
+        this.parentNode.className = "photo";
+        this.onload = null;
+    };
+    
+    img.src = photo.images.thumbnail.url;
+
+    item.setAttribute("data-photo-index", photos_length - 1 );
+    item.className = "photo loading";
+    item.appendChild( img );
+
+    this.photoContainer.append( item );
 };
--- js/PhotoPicker.js
+++ js/PhotoPicker.js
@@ -1,14 +1,43 @@
 function PhotoPicker()
 {
-    this.type        = "photopicker";
-    this.photos      = [];
-    this.nextURL     = null;
-    this.currentURL  = null;
-    this.batchSize   = 10;
+    this.type          = "photopicker";
+    this.photos        = [];
+    this.index         = 0;
+    this.nextURL       = null;
+    this.currentURL    = null;
+    this.batchSize     = 10;
+    this.selectedPhoto = null;
+
+    this.container      = null;
+    this.photoContainer = null;
+    this.loadMoreButton = null;
+    this.cancelButton   = null;
+    this.submitButton   = null;
 }
 
 PhotoPicker.prototype.init = function()
 {
+    var self = this;
+
+    this.photoContainer = this.container.find(".photo-container");
+    this.loadMoreButton = this.container.find(".button-load-more");
+    this.cancelButton   = this.container.find(".button-cancel");
+    this.submitButton   = this.container.find(".button-submit");
+
+    this.photoContainer.on("click", ".photo", $.proxy( this.handlePhotoClick, this ) );
+
+    this.loadMoreButton.click( function() {
+        self.loadMore();
+    });
+
+    this.cancelButton.click( function() {
+        self.clearSelected();
+        self.close();
+    });
+
+    this.submitButton.click( function() {
+        self.handleSubmit();
+    });
 };
 
 PhotoPicker.prototype.open = function()
@@ -17,11 +46,17 @@ PhotoPicker.prototype.open = function()
 
 PhotoPicker.prototype.close = function()
 {
+    console.log("PhotoPicker close");
 };
 
 PhotoPicker.prototype.render = function()
 {
     console.log("Rendering data");
+};
+
+PhotoPicker.prototype.append = function( photo )
+{
+    console.log("Appending photo");
 };
 
 PhotoPicker.prototype.fetchData = function()
@@ -36,6 +71,8 @@ PhotoPicker.prototype.getPhotos = function()
 
 PhotoPicker.prototype.loadMore = function()
 {
+    console.log("PhotoPicker loadMore");
+    this.loadMoreButton.prop("disabled", 1 );
     if ( this.nextURL ) {
         this.fetch( this.nextURL );
     }
@@ -49,6 +86,10 @@ PhotoPicker.prototype.processData = function(data, status, jqXHR)
 PhotoPicker.prototype.fetch = function( url, data )
 {
     this.currentURL = url;
+    data = data || "";
+
+    console.log( url, data );
+
     var xhr = $.ajax({
         url: url,
         data: data,
@@ -58,11 +99,59 @@ PhotoPicker.prototype.fetch = function( url, data )
     ).fail(
         $.proxy( this.fetchFailed, this )
     );
+
     console.log("Fetching data with XHR");
+
     return xhr;
 }
 
 PhotoPicker.prototype.fetchFailed = function()
 {
     console.log("Fetch failed");
+};
+
+PhotoPicker.prototype.clearSelected = function()
+{
+    this.selectedPhoto = null;
+    this.container.find(".selected").removeClass("selected");
+    this.submitButton.prop("disabled", 1 );
+};
+
+PhotoPicker.prototype.handlePhotoClick = function( event )
+{
+    var photo = $(event.currentTarget),
+        photo_index = parseInt( photo.data("photo-index"), 10 );
+
+    if ( photo_index === this.selectedPhoto ) {
+
+        this.clearSelected();
+
+    } else {
+
+        this.clearSelected();
+        photo.addClass("selected");
+        this.selectedPhoto = photo_index;
+        this.submitButton.prop("disabled", 0 );
+
+    }    
+};
+
+PhotoPicker.prototype.getSelectedPhotoURL = function()
+{
+    return "";
+};
+
+PhotoPicker.prototype.getSelectedPhoto = function()
+{
+    if ( this.selectedPhoto !== void 0 ) {
+        return this.photos[ this.selectedPhoto ];
+    }
+    return false;
+};
+
+PhotoPicker.prototype.handleSubmit = function()
+{
+    console.log("PhotoPicker submitted");
+    $(document).trigger("photopicker.photo-selected", [ this.getSelectedPhotoURL(), this.type ] );
+    this.close();
 };
