059e2ad2954b3d3a2669806f8aeeb26f05440a97
Eric King <eric.king@lonelyplanet.com>
2014-11-17 16:39:20 -0600

    Updated popup function. I made a few more window features default to on.

--- js/helpers.js
+++ js/helpers.js
@@ -2,24 +2,33 @@ function popup( url, prop )
 {
     prop = prop || {};
 
-    prop.name   = prop.name || "";
-    prop.width  = prop.width || screen.availWidth / 2;
-    prop.height = prop.height || screen.availHeight / 2;
-    prop.left   = prop.left || (screen.availWidth - prop.width) / 2;
-    prop.top    = prop.top || (screen.availHeight - prop.height) / 2;
-    
-    var features = [];
+    var winName  = prop.name || "",
+        features = [];
+
+    prop.name       = null;
+    prop.width      = prop.width || screen.availWidth / 2;
+    prop.height     = prop.height || screen.availHeight / 2;
+    prop.left       = prop.left || (screen.availWidth - prop.width) / 2;
+    prop.top        = prop.top || (screen.availHeight - prop.height) / 2;
+    prop.resizable  = prop.resizable || 1;
+    prop.scrollbars = prop.scrollbars || 1;
+    prop.status     = prop.status || 1;
 
     for (var i in prop) {
         if ( prop[i] )
             features.push( i + "=" + prop[i] );
     }
 
-    features = features.join(", ");
+    features = features.join(",");
 
-    var win = window.open( url, prop.name, features );
+    var win = window.open( url, winName, features );
 
-    return win && ! win.closed;
+    if ( win && ! win.closed ) {
+        win.focus();
+        return true;
+    }
+
+    return false;
 }
 
 function createObject(proto)
