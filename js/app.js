2ded1724b6439636cd052ff41a9c2252b0a52d0e
Eric King <eric@webdeveric.com>
2014-11-22 22:49:43 -0600

    Added Promise polyfill

--- js/app.js
+++ js/app.js
@@ -2,13 +2,13 @@
 requirejs.config({
     // enforceDefine: true,
     paths: {
-        // jquery: [
-        //     "//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min",
-        //     "../bower_components/jquery/dist/jquery.min"
-        // ],
-        jquery: "../bower_components/jquery/dist/jquery.min",
+        jquery: [
+            "//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min",
+            "../bower_components/jquery/dist/jquery.min"
+        ],
         facebook: "//connect.facebook.net/en_US/all",
-        hogan: "../bower_components/hogan/web/builds/3.0.2/hogan-3.0.2.amd"
+        hogan: "../bower_components/hogan/web/builds/3.0.2/hogan-3.0.2.amd",
+        promise: "../bower_components/es6-promise/promise"
     },
     shim: {
         facebook: {
--- js/polyfills.js
+++ js/polyfills.js
@@ -44,3 +44,46 @@ if ( !Object.keys ) {
         };
     }() );
 }
+
+// Production steps of ECMA-262, Edition 5, 15.4.4.18
+// Reference: http://es5.github.io/#x15.4.4.18
+if ( !Array.prototype.forEach ) {
+
+    Array.prototype.forEach = function( callback, thisArg ) {
+        "use strict";
+
+        var T, k;
+
+        if (this == null) {
+            throw new TypeError("this is null or not defined");
+        }
+
+        var O = Object(this),
+            len = O.length >>> 0;
+
+        if ( typeof callback !== "function" ) {
+            throw new TypeError(callback + " is not a function");
+        }
+
+        if ( arguments.length > 1 ) {
+            T = thisArg;
+        }
+
+        k = 0;
+
+        while (k < len) {
+            var kValue;
+            if ( k in O ) {
+                kValue = O[k];
+                callback.call(T, kValue, k, O);
+            }
+            k++;
+        }
+    };
+
+}
+
+define( [ "promise" ], function( promise ) {
+    "use strict";
+    promise.polyfill();
+});
