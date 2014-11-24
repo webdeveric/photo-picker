2b8de3f185610ce829c593e9cb3b025ebfa2120f
Eric King <eric.king@lonelyplanet.com>
2014-11-24 16:08:49 -0600

    Simplified object storage

--- js/PhotoProvider.js
+++ js/PhotoProvider.js
@@ -10,11 +10,8 @@ define( [
         this.albumsurl = null;
         this.limit     = 40;
 
-        this.photos      = {}; // Photo.id => Photo object
-        this.photosOrder = []; // index => Photo.id
-
-        this.albums      = {}; // Album.id => Album object
-        this.albumsOrder = []; // index => Album.id
+        this.photos    = {}; // Photo.id => Photo object
+        this.albums    = {}; // Album.id => Album object
     }
 
     PhotoProvider.prototype.init = function()
@@ -42,47 +39,20 @@ define( [
 
     PhotoProvider.prototype.numAlbums = function()
     {
-        return this.albumsOrder.length;
+        return Object.keys( this.albums ).length;
     };
 
     PhotoProvider.prototype.numPhotos = function()
     {
-        return this.photosOrder.length;
+        return Object.keys( this.photos ).length;
     };
 
     PhotoProvider.prototype.addPhoto = function( photo_obj )
     {
         if ( photo_obj instanceof Photo ) {
             this.photos[ photo_obj.id ] = photo_obj;
-            this.photosOrder.push( photo_obj.id );
         }
         return this;
-    };
-
-    PhotoProvider.prototype.getPhotosArray = function()
-    {
-        return this._getDataArray( this.photosOrder, this.photos );
-    };
-
-    PhotoProvider.prototype.getAlbumsArray = function()
-    {
-        return this._getDataArray( this.albumsOrder, this.albums );
-    };
-
-    PhotoProvider.prototype._getDataArray = function( order, items )
-    {
-        var i = 0,
-            l = order.length,
-            data = [];
-
-        for ( ; i < l ; ++i ) {
-            var item = items[ order[ i ] ] || false;
-            if ( item ) {
-                data.push( item );
-            }
-        }
-
-        return data;
     };
 
     PhotoProvider.prototype.hasPhoto = function( photo_id )
@@ -90,6 +60,8 @@ define( [
         return this.photos[ photo_id ] && this.photos[ photo_id ] instanceof Photo;
     };
 
+    // Updated this to return a Promise.
+    // If the Photo exists, resolve it, otherwise, call the api to get it.
     PhotoProvider.prototype.getPhoto = function( photo_id )
     {
         return this.hasPhoto( photo_id ) ? this.photos[ photo_id ] : false;
@@ -112,9 +84,8 @@ define( [
 
                 } else {
 
-                    var photos = self.getPhotosArray();
-                    console.info("PhotoProvider.getPhotos < Promise: Photos already loaded", photos );
-                    resolve( photos );
+                    console.info("PhotoProvider.getPhotos < Promise: Photos already loaded", self.photos );
+                    resolve( self.photos );
 
                 }
 
@@ -163,10 +134,10 @@ define( [
         }
 
         if ( this.numAlbums() > 0 ) {
-            return Promise.resolve( this.getAlbumsArray() );
+            return Promise.resolve( this.albums );
         }
 
-        return this.apiScrape( this.albumsurl, { limit: 3 } ).then( $.proxy( this.processAlbumData, this ) );
+        return this.apiScrape( this.albumsurl, this.getParameters() ).then( $.proxy( this.processAlbumData, this ) );
     };
 
     PhotoProvider.prototype.apiGetPhoto = function( photo_id )
@@ -174,17 +145,45 @@ define( [
         return this.api( this.apiGetPhotoURL( photo_id ), this.getParameters() );
     };
 
-    PhotoProvider.prototype.apiGetAlbumURL = function( album_id )
+    PhotoProvider.prototype.apiGetAlbum = function( album_id )
     {
-        return this.api( this.apiGetPhotoURL( album_id ), this.getParameters() );
+        return this.api( this.apiGetAlbumURL( album_id ), this.getParameters() );
+    };
+
+    PhotoProvider.prototype.processResults = function( results )
+    {
+        console.log("PhotoProvider.processResults: Called with results", results );
+
+        this.url = this.getNextUrl( results );
+
+        var i      = 0,
+            data   = results.data,
+            l      = data.length,
+            photos = {}; // This current batch of photos will be returned.
+
+        for ( ; i < l ; ++i ) {
+            var photo = this.buildPhoto( data[ i ] );
+            this.addPhoto( photo );
+            photos[ photo.id ] = photo;
+        }
+
+        console.log("PhotoProvider.processResults: photos ", photos );
+        return photos;
+    };
+
+    PhotoProvider.prototype.processAlbumData = function( data )
+    {
+        console.log("PhotoProvider.processAlbumData: Called with data", data );
+        return [].concat( data );
     };
 
     /*
-        Please override the following functions when you subclass PhotoProvider.
+        Please override the following methods when you subclass PhotoProvider.
     */
 
     PhotoProvider.prototype.api = function( /* url, parameters */ )
     {
+        console.log("PhotoProvider.api: Please override this method in your subclasses");
         return Promise.resolve( {
             data: [],
             paging: {}
@@ -193,43 +192,30 @@ define( [
 
     PhotoProvider.prototype.apiScrape = function( /* url, parameters */ )
     {
+        console.log("PhotoProvider.apiScrape: Please override this method in your subclasses");
         return Promise.reject( new Error("PhotoProvider.apiScrape not implemented.") );
     };
 
     PhotoProvider.prototype.apiGetPhotoURL = function( photo_id )
     {
+        console.log("PhotoProvider.apiGetPhotoURL: Please override this method in your subclasses");
         return "/" + photo_id;
     };
 
     PhotoProvider.prototype.apiGetAlbumURL = function( album_id )
     {
+        console.log("PhotoProvider.apiGetAlbumURL: Please override this method in your subclasses");
         return "/" + album_id;
     };
 
-    PhotoProvider.prototype.processResults = function( results )
+    PhotoProvider.prototype.getNextUrl = function( /* results */ )
     {
-        console.log("PhotoProvider.processResults: Called with results", results );
-        // This is where you set the URL for the next page, if there is one.
-        this.url = results.paging && results.paging.next ? results.paging.next : false;
-
-        var i    = 0,
-            data = results.data,
-            l    = data.length,
-            photos = [];
-
-        for ( ; i < l ; ++i ) {
-            var photo = new Photo();
-            this.addPhoto( photo );
-            photos.push( photo );
-        }
-        console.log("processResults returning", photos );
-        return photos;
+        return false;
     };
 
-    PhotoProvider.prototype.processAlbumData = function( data )
+    PhotoProvider.prototype.buildPhoto = function( /* data */ )
     {
-        console.log("PhotoProvider.processAlbumData: Called with data", data );
-        return [].concat( data );
+        return new Photo();
     };
 
     return PhotoProvider;
