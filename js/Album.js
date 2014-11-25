67dd657f64a3377553280a768783698320facccc
Eric King <eric@webdeveric.com>
2014-11-25 13:47:26 -0600

    Updated Album methods

--- js/Album.js
+++ js/Album.js
@@ -1,23 +1,34 @@
 define( [], function() {
     "use strict";
 
-    function Album( id, name, url, cover_photo )
+    function Album( id, name, photosURL, cover_photo )
     {
         this.id          = id;
         this.name        = name;
-        this.url         = url;
-        this.photos      = [];
+        this.photosURL   = photosURL;
+        this.photoIDs    = [];
         this.cover_photo = cover_photo;
     }
 
-    Album.prototype.addPhoto = function( photo_id ) {
-        this.photos.push( photo_id );
+    Album.prototype.addPhotoID = function( photo_id ) {
+        this.photoIDs.push( photo_id );
         return this;
     };
 
     Album.prototype.getPhotoIDs = function()
     {
-        return this.photos || [];
+        return this.photoIDs || [];
+    };
+
+    Album.prototype.getURL = function()
+    {
+        return this.photosURL || false;
+    };
+
+    Album.prototype.setURL = function( url )
+    {
+        this.photosURL = url;
+        return this;
     };
 
     return Album;
