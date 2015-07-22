4a9af77b68d6243c4e114e85f9077091f66aa3f1
Eric King <eric.king@lonelyplanet.com>
2015-07-22 17:03:43 -0500

    Added getFB helper to load the FB SDK

---
+++ src/facebook-sdk.js
@@ -0,0 +1,67 @@
+import $ from 'jquery';
+
+var loadingSDK = false,
+    resolvers = [],
+    rejecters = [];
+
+function resetVars()
+{
+  loadingSDK = false;
+  resolvers = [];
+  rejecters = [];
+}
+
+function resolveSDK( sdk )
+{
+  resolvers.forEach( ( resolve ) => { resolve( sdk ); } );
+  resetVars();
+}
+
+function rejectSDK( error )
+{
+  rejecters.forEach( ( reject ) => { reject( error ); } );
+  resetVars();
+}
+
+function loadSDK( options = {} )
+{
+  if ( loadingSDK ) {
+    return;
+  }
+
+  loadingSDK = true;
+
+  if ( window.FB ) {
+
+    resolveSDK( window.FB );
+
+  } else {
+
+    $.ajaxSetup({ cache: true });
+
+    $.getScript('//connect.facebook.net/en_US/sdk.js').done( () => {
+
+      if ( Object.keys( options ).length ) {
+        window.FB.init( options );
+      }
+
+      resolveSDK( window.FB );
+
+    }).fail( ( jqxhr, settings, error ) => {
+
+      rejectSDK( new Error( error ) );
+
+    });
+
+  }
+
+}
+
+export default function getFB( { appId = '', version = 'v2.3' } = {} )
+{
+  return new Promise( ( resolve, reject ) => {
+    resolvers.push( resolve );
+    rejecters.push( reject );
+    loadSDK( { appId, version } );
+  });
+}
